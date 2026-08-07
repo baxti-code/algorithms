f4a7fa8179e2ef0957e50becedcfaa544e82169e
Baxtiyor <baxtixolmominov1@gmail.com>
2026-08-07 16:07:21 +0500

    fixed bugs

--- patterns/2_two_pointers/002_3sum/solution.py
+++ patterns/2_two_pointers/002_3sum/solution.py
@@ -14,7 +14,7 @@ class Solution:
         n = len(nums)
         result = []
         
-        for i in nums:
+        for i in range(n):
             if nums[i] > 0:
                 break
             elif i > 0 and nums[i] == nums[i-1]:
@@ -25,15 +25,15 @@ class Solution:
             while low < high:
                 summ = nums[i] + nums[low] + nums[high]
                 if summ == 0:
-                    result.append([nums[i] + nums[low] + nums[high]]) 
+                    result.append([nums[i], nums[low], nums[high]]) 
                     low, high = low +1, high-1
                     while low < high and nums[low] == nums[low-1]:
                         low += 1
-                    while low < high and nums[high] == nums [high+1]:
+                    while low < high and nums[high] == nums[high+1]:
                         high -=1
                 elif summ < 0:
                     low+=1
                 else:
                     high -=1
 
-            return result
+        return result
