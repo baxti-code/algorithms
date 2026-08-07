b97c7025b40cf9c9a525d18cdbf459a8d3b4c520
Baxtiyor <baxtixolmominov1@gmail.com>
2026-08-07 16:00:59 +0500

    add 3sum

---
+++ patterns/2_two_pointers/002_3sum/solution.py
@@ -0,0 +1,39 @@
+"""
+# 15 leetcode
+Difficulty : Easy
+
+Time comp : O(n^2)
+Space comp : O(n)
+
+"""
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        
+        nums.sort()
+        n = len(nums)
+        result = []
+        
+        for i in nums:
+            if nums[i] > 0:
+                break
+            elif i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            low, high = i+1, n-1
+
+            while low < high:
+                summ = nums[i] + nums[low] + nums[high]
+                if summ == 0:
+                    result.append([nums[i] + nums[low] + nums[high]]) 
+                    low, high = low +1, high-1
+                    while low < high and nums[low] == nums[low-1]:
+                        low += 1
+                    while low < high and nums[high] == nums [high+1]:
+                        high -=1
+                elif summ < 0:
+                    low+=1
+                else:
+                    high -=1
+
+            return result
