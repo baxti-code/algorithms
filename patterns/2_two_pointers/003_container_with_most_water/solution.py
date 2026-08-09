34d0a3d091924fc9fba0ebcc801f1fda5b0664c2
baxtiyor <baxtixolmominov1@gmail.com>
2026-08-09 07:50:50 +0500

    Add solution.py

---
+++ patterns/2_two_pointers/003_container_with_most_water/solution.py
@@ -0,0 +1,22 @@
+"""
+#15 leetcode 11. Container With Most Water
+
+Difficulty: Medium
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+"""
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        n = len(height)
+        L, R = 0, n - 1
+        max_area = 0
+        while L < R:
+            area = min(height[L], height[R]) * (R - L)
+            max_area = max(max_area, area)
+            if height[L] < height[R]:
+                L += 1
+            else:
+                R -= 1
+        return max_area 
