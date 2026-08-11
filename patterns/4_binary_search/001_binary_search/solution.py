72783ba8ed440641d0b9f4dac79d0fc42bee58b4
baxtiyor <baxtixolmominov1@gmail.com>
2026-08-11 10:41:28 +0500

    Add binary search

---
+++ patterns/4_binary_search/001_binary_search/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            middle = (l+r) // 2
+
+            if nums[middle] == target:
+                return middle
+
+            elif nums[middle] < target:
+                l = middle + 1
+            else:
+                r = middle - 1
+
+        return -1
+
+
+  
